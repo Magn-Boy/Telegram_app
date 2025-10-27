c70bbc0a29b2af190d90695348b2a4db858d7fad
Magn-Boy <andruha.vasilev.2004@mail.ru>
2025-10-27 12:03:17 +0300

    fix: Changed name app.

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Telegram App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
